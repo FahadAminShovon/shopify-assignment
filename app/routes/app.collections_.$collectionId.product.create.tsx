faf358dab3ea8871ceefab0f854bee6aaaa9f5b6
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2023-12-01 16:13:48 +0600

    add product create

--- app/routes/app.collections_.$collectionId.product.create.tsx
+++ app/routes/app.collections_.$collectionId.product.create.tsx
@@ -1,6 +1,40 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { Card, Layout, Page } from "@shopify/polaris";
 import React from "react";
 import CreateEditForm from "~/components/CreateEditForm";
+import { authenticate } from "~/shopify.server";
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const { admin } = await authenticate.admin(request);
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const description = formData.get("description");
+  const collectionId = `gid://shopify/Collection/${params.collectionId}`;
+
+  await admin.graphql(
+    `#graphql
+		mutation productCreate($input: ProductInput!) {
+		  productCreate(input: $input) {
+		    product {
+		      # Product fields
+					id	
+		    }
+		  }
+		}`,
+    {
+      variables: {
+        input: {
+          title,
+          descriptionHtml: `<p>${description}</p>`,
+          collectionsToJoin: [collectionId],
+        },
+      },
+    }
+  );
+
+  return redirect(`/app/collections/${params.collectionId}/products`);
+}
 
 const CreateProduct = () => {
   return (
