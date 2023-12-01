979df31e21c6585792902eb90b69c13eb36908c0
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2023-12-01 15:50:40 +0600

    add create collection

--- app/routes/app.collection.create.tsx
+++ app/routes/app.collection.create.tsx
@@ -1,6 +1,36 @@
-import { Card, Layout, Page } from "@shopify/polaris";
 import React from "react";
+import { redirect, type ActionFunctionArgs } from "@remix-run/node";
+import { Card, Layout, Page } from "@shopify/polaris";
 import CreateEditForm from "~/components/CreateEditForm";
+import { authenticate } from "~/shopify.server";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const { admin } = await authenticate.admin(request);
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const description = formData.get("description");
+
+  await admin.graphql(
+    `#graphql
+	mutation collectionCreate($title: String!, $description: String ) {
+    collectionCreate(input: {title:$title, descriptionHtml: $description }) {
+      collection {
+        id
+        title
+        description
+			}
+    }
+  }`,
+    {
+      variables: {
+        title,
+        description: `<p>${description}</p>`,
+      },
+    }
+  );
+
+  return redirect("/app/collections");
+}
 
 const CreateCollection = () => {
   return (
